dca6bb9e6fc5fb0eb0ff6e3d44f61b2077ae93ce
RenanCardoso <renancardoso2401@gmail.com>
2021-07-24 18:47:17 -0300

    commitando alteracoes da branch

--- src/resources/views/dashboard/shared/header.blade.php
+++ src/resources/views/dashboard/shared/header.blade.php
@@ -12,7 +12,7 @@
         ?>
         <ul class="c-header-nav ml-auto mr-4">
           <li class="c-header-nav-item dropdown"><a class="c-header-nav-link" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">
-              <div class="c-avatar"><img class="c-avatar-img" src="{{ url('/assets/img/avatars/6.jpg') }}" alt="user@email.com"></div>
+              <div class="c-avatar"><img class="c-avatar-img" src="{{ url('/assets/img/avatars/avatar.png') }}"></div>
             </a>
             <div class="dropdown-menu dropdown-menu-right pt-0">
               <div class="dropdown-header bg-light py-2">
