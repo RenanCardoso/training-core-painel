6e92d61622d463d7aa4bda620228e5ef9ef336ef
RenanCardoso <renancardoso2401@gmail.com>
2021-09-20 19:34:58 -0300

    obrigatoriedade de planos no crud de alunos

--- src/database/migrations/2014_10_12_000000_create_users_table.php
+++ src/database/migrations/2014_10_12_000000_create_users_table.php
@@ -35,7 +35,7 @@ class CreateUsersTable extends Migration
             $table->string('tipousuario', 3);
             $table->string('flaplicativo', 3)->default('nao');
             $table->string('status', 3)->default('ati');
-            $table->integer('plano_id')->nullable();
+            $table->integer('plano_id');
             $table->foreign('plano_id')->references('id')->on('plano');
         });
     }
